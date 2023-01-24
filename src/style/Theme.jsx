b942affee30390048c703513f374f8a0e72f7a77
Jonnycp <jonathan-caputo@hotmail.com>
2023-01-25 00:18:25 +0100

    Theme e GlobalStyle

---
+++ src/style/Theme.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { ThemeProvider } from "styled-components";
+import GlobalStyle from "./GlobalStyle";
+
+const theme = {
+  colors: {
+    bgGreen: "#034A3D",
+    bgLight: "#FFEEE4",
+    primary: "#FD8440",
+    success: "#5CC58C",
+    info: "#1B80B2",
+    gray: "#5A5A5A",
+    warning: "EFAF42",
+    light: "#FFFFFF",
+    dark: "#000000",
+  },
+
+  fonts: {
+    special: '"Lilita One", sans-serif',
+    normal: '"Nunito", sans-serif',
+  },
+};
+
+const Theme = ({ children }) => (
+  <>
+    <GlobalStyle />
+    <ThemeProvider theme={theme}>{children}</ThemeProvider>
+  </>
+);
+
+export default Theme;
