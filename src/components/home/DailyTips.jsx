d338abad3332edcb1793eca3828785a59016d1ef
Luca <73708474+Luca-Landri@users.noreply.github.com>
2023-01-26 00:14:50 +0100

    Feat: Added swiper to the daily tips

--- src/components/home/DailyTips.jsx
+++ src/components/home/DailyTips.jsx
@@ -1,25 +1,34 @@
 import React from 'react'
 import styled from 'styled-components'
 import SingleTip from './SingleTip'
+import 'swiper/css';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import "swiper/css/pagination";
+import { Pagination } from "swiper";
 
 const Container = styled.div`
   margin-top: 70px;
   display: flex;
   flex-direction: column;
   width: 94%;
-  margin-left: auto;
 `
 
 const Title = styled.h2`
   font-family: ${({theme}) => theme.fonts.special};
   font-size: 35px;
   font-weight: 300;
+  margin-left: 20px;
 `
 
 const TipRow = styled.div`
+  margin-top: 20px;
+  display: flex;
+`
+
+const SecondRow = styled.div`
   display: flex;
   flex-direction: row;
-  gap: 20px;
+  justify-content: space-between;
 `
 
 const DailyTips = () => {
@@ -27,10 +36,27 @@ const DailyTips = () => {
     <Container>
       <Title>Daily Tips</Title>
       <TipRow>
-        <SingleTip />
-        <SingleTip />
-        <SingleTip />
-        <SingleTip />
+        <Swiper
+          slidesPerView={3}
+          spaceBetween={30}
+          loop={true}
+          autoplay={{
+            delay: 3000,
+            disableOnInteraction: false
+          }}
+          pagination={true} 
+          modules={[Pagination]}
+        >
+          <SwiperSlide>
+              <SingleTip image="https://cdn.discordapp.com/attachments/1067888696025677885/1067944960546066432/euphorbia-ingens-cactus-candelabro.png" text="Le rose" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <SingleTip image="https://cdn.discordapp.com/attachments/1067888696025677885/1067944960546066432/euphorbia-ingens-cactus-candelabro.png" text="Le rose" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <SingleTip image="https://cdn.discordapp.com/attachments/1067888696025677885/1067944960546066432/euphorbia-ingens-cactus-candelabro.png" text="Le rose" />
+          </SwiperSlide>
+        </Swiper>
       </TipRow>
     </Container>
   )
