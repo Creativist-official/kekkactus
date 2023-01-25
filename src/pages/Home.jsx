43f052740038711fd109cff9cefe47ba51df84c7
Luca <73708474+Luca-Landri@users.noreply.github.com>
2023-01-25 16:41:24 +0100

    Feat: Continued the home
    changed the bg and added the wave, is still to fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,38 @@
 import React from 'react';
 import Header from '../components/Header';
 import styled from 'styled-components';
+import { Icon } from '@iconify/react';
+import Title from '../components/Title';
+
+const Content = styled.div`
+`;
 
 const HomeContainer = styled.section`
    height: 100vh;
    width: 100%;
-   background-color: ${({theme}) => theme.colors.bgGreen}; 
+   background-color: ${({theme}) => theme.colors.bgLight}; 
 `;
+
+const HeaderContainer = styled.section`
+   width: 100%;
+   background-color: ${({theme}) => theme.colors.bgGreen};
+`
 
 const Home = ({}) => {
    return (
       <HomeContainer>
-         <Header />
+         <HeaderContainer>
+            <Header />
+            <Title Title="Da innaffiare"/>
+            <svg viewBox="0 0 397 99" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M397 9.075C353.5 65 94.8734 -5.36156e-06 0 0V99H397V9.075Z" fill="#FFEEE4"/>
+            </svg>
+
+         </HeaderContainer>
+         <Content>
+
+         </Content>
+
       </HomeContainer>
    )
 }
