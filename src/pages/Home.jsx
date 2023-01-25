85819b47314ea5f90eab0d6843b3c37dbfed1418
Luca <73708474+Luca-Landri@users.noreply.github.com>
2023-01-25 22:03:28 +0100

    Fix: Now the wave is positioned correctly

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,9 +3,16 @@ import Header from '../components/Header';
 import styled from 'styled-components';
 import { Icon } from '@iconify/react';
 import Title from '../components/Title';
+import HomePlant from '../components/HomePlant';
 
 const Content = styled.div`
 `;
+
+const SvgWave = styled.svg`
+   position: absolute;
+   width: 100%;
+   top: 50px;
+`
 
 const HomeContainer = styled.section`
    height: 100vh;
@@ -18,19 +25,33 @@ const HeaderContainer = styled.section`
    background-color: ${({theme}) => theme.colors.bgGreen};
 `
 
+const BushImg = styled.img`
+   position: absolute;
+   width: 100%;
+   bottom: -40px;
+`
+
+const RelativeContainer = styled.div`
+   position: relative;
+   height: 150px;
+   width: 100%;
+`
+
 const Home = ({}) => {
    return (
       <HomeContainer>
          <HeaderContainer>
-            <Header />
+            <Header leftIcon="bx:gift" rightIcon='mdi:bell-badge-outline'/>
             <Title Title="Da innaffiare"/>
-            <svg viewBox="0 0 397 99" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M397 9.075C353.5 65 94.8734 -5.36156e-06 0 0V99H397V9.075Z" fill="#FFEEE4"/>
-            </svg>
-
+            <RelativeContainer>
+               <BushImg src="./bush.png" alt="" />
+               <SvgWave viewBox="0 0 397 120" fill="none" xmlns="http://www.w3.org/2000/svg">
+               <path d="M397 9.075C353.5 65 94.8734 -5.36156e-06 0 0V99H397V9.075Z" fill="#FFEEE4"/>
+               </SvgWave>
+            </RelativeContainer>
          </HeaderContainer>
          <Content>
-
+            <HomePlant />
          </Content>
 
       </HomeContainer>
