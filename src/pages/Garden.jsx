03530f1e16068afe05f0b3be914c93165388fd58
Luca <73708474+Luca-Landri@users.noreply.github.com>
2023-01-26 18:26:45 +0100

    Feat: created the header of the garden page

--- src/pages/Garden.jsx
+++ src/pages/Garden.jsx
@@ -2,21 +2,46 @@ import React from 'react'
 import styled from 'styled-components'
 import Title from '../components/Title'
 import Header from '../components/Header'
+import SinglePlant from '../components/garden/SinglePlant'
 
-const Container = styled.div``
+const Container = styled.div`
+  background-color: ${({theme}) => theme.colors.bgLight};
+`
 
 const HeaderContainer = styled.div`
     background-color: ${({theme}) => theme.colors.bgGreen};
+`
+
+const RelativeContainer = styled.div`
+  position: relative;
+  width: 100%;
+`
+
+const SvgWave = styled.svg`
+   position: absolute;
+   width: 100%;
+   bottom: -1px;
+`
+
+const TitleContainer = styled.div`
+  padding-bottom: 12px;
 `
 
 const Garden = () => {
   return (
     <Container>
         <HeaderContainer>
+          <RelativeContainer>
             <Header leftIcon="bx:gift" rightIcon='mdi:bell-badge-outline' doubleSet={false}/>
-            <Title Title="Garden" icon={true}/>
+            <TitleContainer>
+              <Title Title="Garden" icon={true}/>
+            </TitleContainer>
+            <SvgWave width="100%" viewBox="0 0 390 29" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M390 0C338.5 29 92.5 15 0 17.5V29H390V0Z" fill="#FFEEE4"/>
+            </SvgWave>
+          </RelativeContainer>
         </HeaderContainer>
-        <h1>Titolone</h1>
+        <SinglePlant />
     </Container>
   )
 }
