164c587eaa11f6ca8bc61e438d79062a19474882
Jonnycp <jonathan-caputo@hotmail.com>
2023-01-26 02:36:25 +0100

    SendResponse function
    Parameters:
    - statusCode
    - message
    - extra array

---
+++ backend/utils/returnMessage.php
@@ -0,0 +1,12 @@
+<?php
+function sendResponse($statusCode,$message,$extra = []){
+    $json = array_merge([
+        'success' => $statusCode >= 200 && $statusCode < 300 ? true : false,
+        'statusCode' => $statusCode,
+        'message' => $message
+    ],$extra);
+
+    http_response_code($statusCode);
+    echo json_encode($json);
+    die();
+}
