9ee52a1e9ec06f877af15d927f65cdc0d279419a
Jonnycp <jonathan-caputo@hotmail.com>
2023-01-26 10:49:30 +0100

    Adedd strip_tags

--- backend/utils/checkInputs.php
+++ backend/utils/checkInputs.php
@@ -2,25 +2,11 @@
     function cleanInputs($input, $regex=null) {
         $input = trim($input);
         $input = stripslashes($input);
+        $input = strip_tags($input);
         $input = htmlspecialchars($input);
         $input = str_replace(' ', '', $input);
         $regex != null && $input = preg_replace($regex, '', $input);
         return $input;
         //'/[^A-Za-z0-9\-]/'
-    }
-
-    function controlliInput($input, $min=null, $max=null) {
-        if(empty($input)) {
-            return false;
-        }
-
-        if($min != null && $input < $min) {
-            return false;
-        }
-        if($max != null && $input > $max) {
-            return false;
-        }
-
-        return true;
     }
 ?>
