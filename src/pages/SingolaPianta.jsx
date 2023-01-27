09d562fcc2676574e2e19e6205df94ddda7fd120
AtonksAlien <losaccoandrea04@gmail.com>
2023-01-27 12:44:11 +0100

    Update SingolaPianta.jsx
    Update SingolaPianta.jsx

--- src/pages/SingolaPianta.jsx
+++ src/pages/SingolaPianta.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 import { Icon } from '@iconify/react';
 import Spec from '../components/Spec';
+import InfoesCard from '../components/InfoesCard';
+import Data from '../components/Data';
 
 const Container = styled.div`
     display: flex;
@@ -69,12 +71,67 @@ const Calendar = styled.div`
     justify-content: center;
     align-items: center;
     width: 100%;
+    padding-bottom: 12%;
     border-top: 1px solid ${props => props.theme.colors.gray};
+    border-bottom: 1px solid ${props => props.theme.colors.gray};
     `;
 
 const Card = styled.div`
     display: flex;
     `;
+
+const Stats = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-top: 5%;
+    margin-left: 5%;
+    width: 90%;
+    justify-content: center;
+    & h3{
+        font-family: ${props => props.theme.fonts.normal};
+        font-weight: 900;
+        font-size: 20px;
+    }
+`;
+
+const Datas = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+    width: 100%;
+    margin-top: 5%;
+    
+`;
+
+const QeA = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-top: 5%;
+    width: 100%;
+    border-top: 1px solid ${props => props.theme.colors.gray};
+    & h3{
+        font-family: ${props => props.theme.fonts.normal};
+        font-weight: 900;
+        font-size: 20px;
+        margin-top: 3%;
+        margin-bottom: 2%;
+        margin-left: 3%;
+    }
+    & div{
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        margin-left: 3%;
+        font-weight: 700;
+        font-size: 20px;
+        margin-right: 5%;
+        font-family: ${props => props.theme.fonts.normal};
+        text-decoration: underline;
+        & p{
+            margin-right: 3%;
+        }
+    }
+`;
 
 const SingolaPianta = ({props}) => {
    return   <Container>
@@ -82,7 +139,7 @@ const SingolaPianta = ({props}) => {
                 <FotoPianta>
                     <img src="/pianta1.png" alt="Foto pianta" />
                     <svg viewBox="0 0 393 27" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M109.5 15.5C66 15.5 27.3333 10.8334 0 2.00009V26.5001H392.5C278 -24.4999 160.5 15.5 109.5 15.5Z" fill="#FFEEE4"/>
+                        <path d="M109.5 15.5C66 15.5 27.3333 10.8334 0 2.00009V26.5001H392.5C278 -24.4999 160.5 15.5 109.5 15.5Z" fill="#FFEEE4"/>
                     </svg>
                 </FotoPianta>
 
@@ -127,8 +184,40 @@ const SingolaPianta = ({props}) => {
                     />
                 </Specs>
                 <Calendar>
-                    <Card/>
+                    <InfoesCard/>
                 </Calendar>
+                <Stats>
+                    <h3>Statistiche</h3>
+                    <Datas>
+                        <Data
+                            icon="mdi:arrow-left-right"
+                            value="1 m"
+                            what="Altezza"
+                            rotate={1}
+                        />
+                        <Data
+                            icon="material-symbols:water-drop"
+                            value="10 l"
+                            what="Acqua usata"
+                            rotate={0}
+                        />
+                        <Data
+                            icon="material-symbols:medical-information"
+                            value="0"
+                            what="Cure"
+                            rotate={0}
+                        />
+                    </Datas>
+                </Stats>
+                <QeA>
+                    <h3>Pensi che la tua pianta stia male?</h3>
+                    <div>
+                        <p>
+                            Identifica la malattia
+                        </p>
+                        <Icon width={26} color="#000" icon="material-symbols:arrow-forward-ios-rounded" />
+                    </div>
+                </QeA>
             </Container>
 }
 
