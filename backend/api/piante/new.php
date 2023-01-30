f83e7603b00e1288fdeeb9e941f93a52ae71e5e7
Jonnycp <jonathan-caputo@hotmail.com>
2023-01-30 07:33:08 +0100

    Check image

--- backend/api/piante/new.php
+++ backend/api/piante/new.php
@@ -12,6 +12,7 @@
     $data = json_decode(file_get_contents("php://input"));
     $auth = new Auth($conn, getallheaders());
     $user = $auth->isValid();
+    $upload = __DIR__."/../../../uploads/";
 
     if($user){
         if($_SERVER["REQUEST_METHOD"] != "POST"){
@@ -24,7 +25,7 @@
             $exactId = cleanInputs($data->exactId);
             $born = cleanInputs($data->born);
             $date = explode("-", $born); //YYYY-MM-DD
-            
+
             $image = $_FILES["image"];
 
             if(empty($nickname) || empty($category) || empty($exactId) || empty($born)){
@@ -37,6 +38,12 @@
                 sendResponse(422, "ID del tipo di pianta non valido");
             }else if(checkdate($date[1], $date[2], $date[0])){
                 sendResponse(422, "Data di nascita non valida");
+            }else{
+                if(empty($image)){
+                    sendResponse(400, "Devi caricare un immagine");
+                }else{
+
+                }
             }
         }
     } else {
