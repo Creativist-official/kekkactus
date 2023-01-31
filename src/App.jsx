139c114eb2e2efd0e0e4d856aea6eda147f0c1f9
Jonnycp <jonathan-caputo@hotmail.com>
2023-01-31 02:35:19 +0100

    Provider, ToastContainer and new Route
    - Added redux provider
    - Added Toast container with setup (bottom-center, colored, 5000ms)
    - Redirect from /home to /
    - Route /recover-password

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+import { Provider } from "react-redux";
+import store from "./logic/store";
+
 import Theme from "./style/Theme";
 import OnlyMobile from "./style/OnlyMobile";
 import Garden from "./pages/Garden";
@@ -12,15 +17,31 @@ const App = () => {
     <>
       <Theme>
         <OnlyMobile>
+          <Provider store={store}>
+          <ToastContainer
+            position="bottom-center"
+            autoClose={5000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="colored"
+          />
           <BrowserRouter>
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/login" element={<Login />} />
               <Route path="/register" element={<Register />} />
-              <Route path="/garden" element={<Garden/>}/>
+              <Route path="/garden" element={<Garden />} />
+              <Route path="/recover-password" element={<h1>Ricordatela.</h1>} />
+              <Route path="/home" element={<Navigate to="/"/>} />
               <Route path="*" element={<h1>404</h1>} />
             </Routes>
           </BrowserRouter>
+          </Provider>
         </OnlyMobile>
       </Theme>
     </>
